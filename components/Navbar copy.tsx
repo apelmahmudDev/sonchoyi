7edd330a64c85286b93c978592a81ad5f5b337a0
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-01-31 00:18:01 +0600

    Refactor theme and navbar components

---
+++ components/Navbar copy.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState, useMemo } from "react";
+import { Button, PaletteMode } from "@mui/material";
+
+const Navbar = () => {
+	const [mode, setMode] = useState<PaletteMode>("light");
+
+	const colorMode = useMemo(
+		() => ({
+			// The dark mode switch would invoke this method
+			toggleColorMode: () => {
+				setMode((prevMode: PaletteMode) =>
+					prevMode === "light" ? "dark" : "light"
+				);
+			},
+		}),
+		[]
+	);
+
+	return (
+		<Button
+			variant="contained"
+			onClick={colorMode.toggleColorMode}
+			sx={{ mb: 2 }}
+		>
+			Theme
+		</Button>
+	);
+};
+
+export default Navbar;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,110 @@
-"use client";
-import { useState, useMemo } from "react";
-import { Button, PaletteMode } from "@mui/material";
+'use client'
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import MenuIcon from '@mui/icons-material/Menu'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
-const Navbar = () => {
-	const [mode, setMode] = useState<PaletteMode>("light");
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window
+}
 
-	const colorMode = useMemo(
-		() => ({
-			// The dark mode switch would invoke this method
-			toggleColorMode: () => {
-				setMode((prevMode: PaletteMode) =>
-					prevMode === "light" ? "dark" : "light"
-				);
-			},
-		}),
-		[]
-	);
+const drawerWidth = 240
+const navItems = ['Home', 'About', 'Contact']
 
-	return (
-		<Button
-			variant="contained"
-			onClick={colorMode.toggleColorMode}
-			sx={{ mb: 2 }}
-		>
-			Theme
-		</Button>
-	);
-};
+export default function Navbar(props: Props) {
+  const { window } = props
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
-export default Navbar;
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState)
+  }
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Fundwave
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Fundwave
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  )
+}
