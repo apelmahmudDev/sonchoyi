941169ee578c1ad659a23f5e2bda78b5c43cb27d
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-05-02 23:24:53 +0600

    [feat] Add dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Paper } from "@mui/material";
+
+export default function DashboardPage() {
+    return (
+        <div style={{margin:10}}>
+            <Paper>
+                <h2>Recent Activity</h2>
+                <p>There is no recent activity.</p>
+            </Paper>
+        </div>
+    )
+}
