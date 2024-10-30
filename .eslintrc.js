c40a378f1fb6f918caf7e2c85210c966963297fd
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-10-30 22:03:30 +0600

    [refactor] Remove unused files and components to clean up the project

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
-}
--- app/store/StoreProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { Provider } from 'react-redux'
-import { store } from '.'
-
-const StoreProvider = (props: { children: React.ReactNode }) => {
-  return <Provider store={store}>{props.children}</Provider>
-}
-
-export default StoreProvider
