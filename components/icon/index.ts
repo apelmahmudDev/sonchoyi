d01196877a83ad1f9c26c8dc30b05b906f2c5045
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-11-02 20:49:48 +0600

    [feat] Add ArrowRightIcon and ThreeLineIcon components

--- components/icon/index.ts
+++ components/icon/index.ts
@@ -10,4 +10,6 @@ export { default as ShoppingBagIcon } from "./ShoppingBagIcon";
 export { default as NotificationIcons } from "./NotificationsIcon";
 export { default as RestaurantIcon } from "./RestaurantIcon";
 export { default as RecurringBillIcon } from "./RecurringBillIcon";
+export { default as ThreeLineIcon } from "./ThreeLineIcon";
 export { default as ArrowLeftIcon } from "./ArrowLeftIcon";
+export { default as ArrowRightIcon } from "./ArrowRightIcon";
