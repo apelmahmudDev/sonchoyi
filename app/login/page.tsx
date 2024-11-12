812ed675c90b785589dabce558d40fd96f25f9fa
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-11-12 22:12:58 +0600

    [feat] implement Google authentication and update login page; add .env.example and modify .gitignore

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
+	const handleAuth = (event) => {
+		event.preventDefault();
+		signIn("google", { callbackUrl: "http://localhost:3000" });
+	};
 	return (
 		<div className="p-4">
 			<p className="font-semibold text-lg text-center my-5">Login</p>
@@ -14,6 +20,11 @@ export default function LoginPage() {
 			<p className="font-semibold text-lg text-center mt-[33px] mb-[38px]">
 				Forgot Password?
 			</p>
+			<div className="flex justify-center">
+				<button type="button" onClick={handleAuth} className="mb-5">
+					Sign with google
+				</button>
+			</div>
 			<p className="text-center text-[#91919F]">
 				Don’t have an account yet?{" "}
 				<Link className="text-[#7F3DFF]" href="/signup">
