498a7d528cc3c71a6c538876a8f7b18b96c454d3
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-11-27 22:17:59 +0600

    [feat] Redirect authenticated users to account page from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import { Carousel } from "@/components/home";
 
-export default function Home() {
+export default async function Home() {
+	const session = await auth();
+	if (session?.user) return redirect("/account");
 	return <Carousel />;
 }
