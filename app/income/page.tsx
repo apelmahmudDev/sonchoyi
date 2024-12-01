6615681c0dabf3b9c497568470d595497cc02dc6
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-12-01 23:35:18 +0600

    [feat] Refactor IncomePage to use async/await for data fetching and streamline income submission process

--- app/income/page.tsx
+++ app/income/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { auth } from "@/auth";
 import { ArrowLeftIcon } from "@/components/icon";
 import { Button } from "@/components/ui/button";
@@ -11,40 +10,74 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { getSubAccountsByUserId, getUserByEmail } from "@/database/queries";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
 
-export default function IncomePage() {
-	const router = useRouter();
-	const session = auth();
-	const [incomeInfo, setIncomeInfo] = useState(null);
-	const handleNavigateBack = () => {
-		router.push("/account");
-	};
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		fetch("/api/income", {
+export default async function IncomePage() {
+	const session = await auth();
+	const user = await getUserByEmail(session?.user?.email as string);
+	const subAccounts = await getSubAccountsByUserId(user?.id);
+	// console.log(subAccounts);
+
+	// const router = useRouter();
+	// const [incomeInfo, setIncomeInfo] = useState(null);
+	// const handleNavigateBack = () => {
+	// 	router.push("/account");
+	// };
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	fetch("/api/income", {
+	// 		method: "POST",
+	// 		body: JSON.stringify(incomeInfo),
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	})
+	// 		.then((res) => {
+	// 			console.log(res);
+	// 			router.push("/account");
+	// 		})
+	// 		.catch((error) => {
+	// 			console.error("Error:", error);
+	// 		});
+	// };
+
+	const addIncome = async (formData) => {
+		"use server";
+
+		const incomeData = {
+			userId: user?.id,
+			accountId: formData.get("accountId"),
+			category: formData.get("category"),
+			amount: formData.get("amount"),
+			description: formData.get("description"),
+		};
+
+		const response = await fetch("/api/income", {
 			method: "POST",
-			body: JSON.stringify(incomeInfo),
+			body: JSON.stringify(incomeData),
 			headers: {
 				"Content-Type": "application/json",
 			},
-		})
-			.then((res) => {
-				console.log(res);
-				router.push("/account");
-			})
-			.catch((error) => {
-				console.error("Error:", error);
-			});
+		});
+
+		if (response.ok) {
+			router.push("/account");
+		} else {
+			console.error("Error:", response.statusText);
+		}
 	};
 
 	return (
-		<div className="flex flex-col p-0 h-screen">
+		<form onSubmit={addIncome} className="flex flex-col p-0 h-screen">
 			{/* how much amount to be add */}
 			<div className="bg-[#00A86B] px-4 pt-4 pb-8">
 				<div className="flex items-center mb-[60px]">
-					<button onClick={handleNavigateBack} className="text-white">
+					<button
+						//  onClick={handleNavigateBack}
+						className="text-white"
+					>
 						<ArrowLeftIcon />
 					</button>
 					<p className="mx-auto text-center text-white text-lg font-semibold pr-8">
@@ -60,6 +93,7 @@ export default function IncomePage() {
 							$
 						</label>
 						<Input
+							name="amount"
 							defaultValue="0"
 							type="number"
 							className="bg-transparent text-[#FCFCFC] text-[64px] font-semibold w-full min-w-200 focus:outline-none outline-none border-none p-1"
@@ -70,74 +104,85 @@ export default function IncomePage() {
 
 			{/* settings options */}
 			<div className="flex-1 bg-white p-4 relative -top-5 rounded-t-2xl">
-				<form onSubmit={handleSubmit}>
-					{/* select - income category */}
-					<Select
-						onValueChange={(value) =>
-							setIncomeInfo((prev) => ({ ...prev, category: value }))
-						}
+				{/* <form action={handleSubmit}> */}
+				{/* select - income category */}
+				<Select
+					// onValueChange={(value) =>
+					// 	setIncomeInfo((prev) => ({ ...prev, category: value }))
+					// }
+					// onValueChange={(value) => console.log(value)}
+					name="category"
+				>
+					<SelectTrigger
+						// onChange={() => {
+						// 	console.log("click");
+						// }}
+						// onChange={(e) => console.log(e)}
+						// name="category"
+						className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none"
 					>
-						<SelectTrigger
-							onChange={() => {
-								console.log("click");
-							}}
-							className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none"
-						>
-							<SelectValue placeholder="Category" />
+						<SelectValue placeholder="Category" />
+					</SelectTrigger>
+					<SelectContent className="h-[200px]">
+						<SelectGroup>
+							<SelectItem value="salary">Salary</SelectItem>
+							<SelectItem value="bonus">Bonus</SelectItem>
+							<SelectItem value="interest">Interest</SelectItem>
+							<SelectItem value="business">Business</SelectItem>
+							<SelectItem value="investments">Investments</SelectItem>
+							<SelectItem value="freelance">Freelance</SelectItem>
+							<SelectItem value="savings ">Savings</SelectItem>
+							<SelectItem value="pension ">Pension</SelectItem>
+							<SelectItem value="tutoring ">Tutoring</SelectItem>
+							<SelectItem value="selling ">Selling Goods</SelectItem>
+							<SelectItem value="other">Other</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+				<Input
+					// onChange={(e) =>
+					// 	setIncomeInfo((prev) => ({
+					// 		...prev,
+					// 		description: e.target.value,
+					// 	}))
+					// }
+					name="description"
+					placeholder="Description"
+					className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none mt-4"
+				/>
+				{/* select - account type*/}
+				<div className="mt-4">
+					<Select
+						// onValueChange={(value) =>
+						// 	setIncomeInfo((prev) => ({ ...prev, type: value }))
+						// }
+						name="accountId"
+					>
+						<SelectTrigger className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none">
+							<SelectValue placeholder="Account" />
 						</SelectTrigger>
-						<SelectContent className="h-[200px]">
+						<SelectContent>
 							<SelectGroup>
-								<SelectItem value="salary">Salary</SelectItem>
-								<SelectItem value="bonus">Bonus</SelectItem>
-								<SelectItem value="interest">Interest</SelectItem>
-								<SelectItem value="business">Business</SelectItem>
-								<SelectItem value="investments">Investments</SelectItem>
-								<SelectItem value="freelance">Freelance</SelectItem>
-								<SelectItem value="savings ">Savings</SelectItem>
-								<SelectItem value="pension ">Pension</SelectItem>
-								<SelectItem value="tutoring ">Tutoring</SelectItem>
-								<SelectItem value="selling ">Selling Goods</SelectItem>
-								<SelectItem value="other">Other</SelectItem>
+								{/* <SelectItem value="cash">Cash</SelectItem>
+								<SelectItem value="bank">Bank</SelectItem>
+								<SelectItem value="credit">Credit</SelectItem> */}
+								{subAccounts?.map((account) => (
+									<SelectItem key={account?.id} value={account?.id}>
+										{account?.accountName}
+									</SelectItem>
+								))}
 							</SelectGroup>
 						</SelectContent>
 					</Select>
-					<Input
-						onChange={(e) =>
-							setIncomeInfo((prev) => ({
-								...prev,
-								description: e.target.value,
-							}))
-						}
-						placeholder="Description"
-						className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none mt-4"
-					/>
-					{/* select - account type*/}
-					<div className="mt-4">
-						<Select
-							onValueChange={(value) =>
-								setIncomeInfo((prev) => ({ ...prev, type: value }))
-							}
-						>
-							<SelectTrigger className="w-full h-[56px] rounded-xl font-medium border-[#F1F1FA] shadow-none">
-								<SelectValue placeholder="Account" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectGroup>
-									<SelectItem value="cash">Cash</SelectItem>
-									<SelectItem value="bank">Bank</SelectItem>
-									<SelectItem value="credit">Credit</SelectItem>
-								</SelectGroup>
-							</SelectContent>
-						</Select>
-					</div>
-					<Button
-						type="submit"
-						className="w-full h-[56px] rounded-xl font-medium bg-[#7F3DFF] text-white mt-4"
-					>
-						Add Income
-					</Button>
-				</form>
+				</div>
+				<Button
+					type="submit"
+					className="w-full h-[56px] rounded-xl font-medium bg-[#7F3DFF] text-white mt-4"
+				>
+					Add Income
+				</Button>
+				{/* </form> */}
 			</div>
-		</div>
+		</form>
 	);
 }
