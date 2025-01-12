af63842a9bfb31e66743a8e7e8de13d32bd0c70a
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-01-12 19:44:44 +0600

    [feat] Add Toaster component for notifications and integrate it into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { connectToDatabase } from "@/service/mongo";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default async function RootLayout({
 	await connectToDatabase();
 	return (
 		<html lang="en">
-			<body className={`${inter.className} light`}>{children}</body>
+			<body className={`${inter.className} light`}>
+				{children}
+				<Toaster />
+			</body>
 		</html>
 	);
 }
