10903923ba29cbbf9437bea80461996ab7f44ca4
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-01-12 21:44:03 +0600

    [feat] Add form handling with Zod validation for login and signup pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,24 +6,46 @@ import { Input } from "@/components/ui/input";
 import { signIn } from "next-auth/react";
 import { login } from "../../actions";
 import { useRouter } from "next/navigation";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+	email: z.string().email({ message: "Invalid email address." }),
+	password: z.string().min(6, {
+		message: "Password must be at least 6 characters.",
+	}),
+});
 
 export default function LoginPage() {
 	const router = useRouter();
 	const [error, setError] = useState<string | null>(null);
 
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
 	const handleAuth = () => {
 		signIn("google", { callbackUrl: "http://localhost:3000/letsgo" });
 	};
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const email = formData.get("email") as string;
-		const password = formData.get("password") as string;
+	async function onSubmit(values: z.infer<typeof formSchema>) {
 		try {
 			const response = await login({
-				email,
-				password,
+				email: values.email,
+				password: values.password,
 			});
 			if (!!response.error) {
 				setError(response.error?.message);
@@ -33,7 +55,7 @@ export default function LoginPage() {
 		} catch (error) {
 			setError(error as string);
 		}
-	};
+	}
 
 	return (
 		<div className="p-4">
@@ -43,11 +65,35 @@ export default function LoginPage() {
 					<p className="text-red-500 text-center text-sm">{error?.message}</p>
 				)}
 			</div>
-			<form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-				<Input name="email" type="email" placeholder="Email" />
-				<Input name="password" type="password" placeholder="Password" />
-				<Button type="submit">Login</Button>
-			</form>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input type="text" placeholder="Email " {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input type="password" placeholder="Password" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">Login</Button>
+				</form>
+			</Form>
 			<p className="font-semibold text-lg text-center mt-[33px] mb-[38px]">
 				Forgot Password?
 			</p>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,40 +1,69 @@
 "use client";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+	name: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+	email: z.string().email({ message: "Invalid email address." }),
+	password: z.string().min(6, {
+		message: "Password must be at least 6 characters.",
+	}),
+});
 
 export default function SignUpPage() {
 	const router = useRouter();
 	const [error, setError] = useState<string | null>(null);
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		try {
-			const formData = new FormData(e.currentTarget);
-			const name = formData.get("name");
-			const email = formData.get("email");
-			const password = formData.get("password");
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+		},
+	});
 
+	async function onSubmit(values: z.infer<typeof formSchema>) {
+		try {
 			const res = await fetch("/api/auth/register", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({
-					name,
-					email,
-					password,
+					name: values.name,
+					email: values.email,
+					password: values.password,
 				}),
 			});
 			if (res.status === 201) {
 				router.push("/login");
+			} else {
+				setError({
+					message: "There were somethings error",
+				});
 			}
 		} catch (error) {
 			setError(error as string);
 		}
-	};
+	}
 
 	return (
 		<div className="p-4">
@@ -44,12 +73,47 @@ export default function SignUpPage() {
 					<p className="text-red-500 text-center text-sm">{error?.message}</p>
 				)}
 			</div>
-			<form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-				<Input name="name" type="text" placeholder="Name" />
-				<Input name="email" type="email" placeholder="Email" />
-				<Input name="password" type="password" placeholder="Password" />
-				<Button type="submit">Sign In</Button>
-			</form>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<FormField
+						control={form.control}
+						name="name"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input type="text" placeholder="Name " {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input type="text" placeholder="Name " {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input type="password" placeholder="Password" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit">Sign In</Button>
+				</form>
+			</Form>
 			<p className="font-semibold text-lg text-center mt-[33px] mb-[38px]">
 				Sign Up with Google
 			</p>
