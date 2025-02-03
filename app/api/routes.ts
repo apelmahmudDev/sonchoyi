6e23fd821a7507a1e76191c45985154db905e8a7
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-02-03 22:54:38 +0600

    [feat] Add API route constants and public routes list

---
+++ app/api/routes.ts
@@ -0,0 +1,16 @@
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const ROOT = "/";
+
+export const PUBLIC_ROUTES = [
+	"/login",
+	"/register",
+	"/forgot-password",
+	"/reset-password",
+	"/terms-and-conditions",
+	"/privacy-policy",
+	"/faq",
+	"/about-us",
+	"/contact-us",
+	"/coming-soon",
+];
