f854a10da0f061a263eaf94860dbcc6d1af9c69c
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-02-25 21:47:46 +0600

    [feat] Add google provider for authentication

--- auth.ts
+++ auth.ts
@@ -1,53 +1,18 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-import GoogleProvider from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
-// import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import { userModel } from "./models/user-model";
-import bcrypt from "bcryptjs";
-// import client from "./lib/db";
+import Google from "next-auth/providers/google";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-	// adapter: MongoDBAdapter(client, { databaseName: process.env.ENVIRONMENT }),
-	...authConfig,
+export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [
-		Credentials({
-			credentials: {
-				email: {},
-				password: {},
-			},
-			authorize: async (credentials) => {
-				let user = null;
-				if (credentials === null) return user;
-				try {
-					user = await userModel.findOne({ email: credentials.email });
-					if (user) {
-						const isMatch = await bcrypt.compare(
-							credentials.password,
-							user.password
-						);
-						if (isMatch) {
-							return user;
-						} else {
-							throw new Error("Email or password is incorrect.");
-						}
-					} else {
-						throw new Error("No user found");
-					}
-				} catch {
-					throw new Error("Invalid credentials.");
-				}
-				return user;
-			},
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		Google({
+			clientId: process.env.AUTH_GOOGLE_ID,
+			clientSecret: process.env.AUTH_GOOGLE_SECRET,
 			authorization: {
-				prompt: "consent",
-				access_type: "offline",
-				response_type: "code",
-			}
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+				},
+			},
 		}),
 	],
 });
