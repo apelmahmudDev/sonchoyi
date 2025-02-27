17e8cab649cda9b74524681e7558840ead32d719
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-02-27 21:39:35 +0600

    [feat] Connect db to mongodb altas

--- service/mongo.ts
+++ service/mongo.ts
@@ -3,8 +3,12 @@ import mongoose from "mongoose";
 export async function connectToDatabase() {
 	try {
 		const connect = await mongoose.connect(
-			String(process.env.MONGO_CONNECTION_STRING)
+			String(process.env.MONGO_CONNECTION_STRING),
+			{
+				dbName: "fundwave", // ✅ Ensure it uses the "fundwave" database
+			}
 		);
+		console.log("Connected to MongoDB - fundwave database");
 		return connect;
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error);
