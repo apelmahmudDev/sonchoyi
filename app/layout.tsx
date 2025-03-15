fd000dcbbefb23dc76bd92ddd285a040e8c26df8
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-03-15 21:13:48 +0600

    [fix] Fix dark theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
