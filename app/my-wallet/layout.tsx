5fcb474f71a3c214524c720b5b741e6265bf7166
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-04-30 22:24:45 +0600

    [feat] Add margin bottom in dashboard layout

--- app/my-wallet/layout.tsx
+++ app/my-wallet/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
 			<AppSidebar />
 			<main className="font-[family-name:var(--font-inter)] w-full">
 				<DashboardHeader />
-				<div className="max-w-[1456px] mx-auto px-4 xl:px-10 py-2">
+				<div className="max-w-[1456px] mx-auto px-4 xl:px-10 pt-2 pb-12">
 					{children}
 				</div>
 			</main>
